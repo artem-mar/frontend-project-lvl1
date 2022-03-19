c3df9d5d98db7b758e0b8eb542105a1f66938ba1
Artem M <artemmarin91@mail.ru>
2022-03-19 20:31:29 +0300

    refactoring scr/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import readlineSync from 'readline-sync';
 
-export const playGame = (gameRules, question, check, correctAnswer) => {
-  console.log(gameRules);
+export const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${userName}`);
+  return userName;
+};
 
+export const getRandomInt = (min, max) => {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+const exam = (userValue, correctValue) => userValue === correctValue;
+
+export const playGame = (question, check, correctAnswer) => {
   console.log(`Question: ${question}`);
   const userAnswer = readlineSync.question('Your answer: ');
   let result = null;
@@ -18,7 +30,19 @@ export const playGame = (gameRules, question, check, correctAnswer) => {
   return result;
 };
 
-export const getRandomInt = (min, max) => {
-  const rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+export const gameLauncher = (name, gameRules, reloader) => {
+  let counter = 0;
+  console.log(gameRules);
+  while (counter < 3) {
+    const [expression, result] = reloader();
+    if (playGame(expression, exam, result)) {
+      counter += 1;
+    } else {
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+  }
+  if (counter === 3) {
+    console.log(`Congratulations, ${name}`);
+  }
 };
