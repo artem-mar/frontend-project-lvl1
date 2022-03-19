354d38df559fcd1dd4edacada2941010073abd19
Artem M <artemmarin91@mail.ru>
2022-03-19 20:30:30 +0300

    refactoring bin/brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,19 +1,14 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import { playGame, getRandomInt } from '../src/index.js';
-
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name?: ');
-console.log(`Hello, ${userName}`);
+import { getRandomInt, greeting, gameLauncher } from '../src/index.js';
 
 const description = 'What is the result of the expression?';
-let expression = null;
-let result = null;
 
 const reloadData = () => {
-  const a = getRandomInt(0, 100);
-  const b = getRandomInt(0, 100);
+  const a = getRandomInt(0, 10);
+  const b = getRandomInt(0, 10);
+  let expression = null;
+  let result = null;
 
   switch (getRandomInt(1, 3)) {
     case 1:
@@ -31,21 +26,20 @@ const reloadData = () => {
     default:
       break;
   }
-};
-reloadData();
-const exam = (userValue, correctValue) => userValue === correctValue;
-
-const gameLauncher = (name, reloader) => {
-  let counter = 0;
-  while (counter < 3) {
-    if (playGame(description, expression, exam, result)) {
-      counter += 1;
-      reloader();
-    } else break;
-  }
-  if (counter === 3) {
-    console.log(`Congratulations, ${name}`);
-  }
+  return [expression, result];
 };
 
-gameLauncher(userName, reloadData);
+const userName = greeting();
+gameLauncher(userName, description, reloadData);
+
+// еще 1 способ написания функции reloadData()
+// const reloadData = () => {
+//   const operators = ['+', '-', '*'];
+//   const firstValue = getRandomInt(0, 10);
+//   const secondValue = getRandomInt(0, 10);
+//   const operator = operators[getRandomInt(0, operators.length - 1)];
+
+//   const expression = `${firstValue} ${operator} ${secondValue}`;
+//   const result = `${eval(expression)}`;
+//   return [expression, result];
+// };
