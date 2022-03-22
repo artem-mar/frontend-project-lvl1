ee7bf5911e1c1064c06a38986b2df35beaded7fd
Artem M <artemmarin91@mail.ru>
2022-03-23 01:13:48 +0300

    refactoring src/games/brainEvenLogic.js

--- src/games/brainEvenLogic.js
+++ src/games/brainEvenLogic.js
@@ -1,20 +1,15 @@
 import gameLauncher from '../index.js';
 import getRandomInt from '../getRandomInt.js';
-import greeting from '../cli.js';
+
+const generateData = () => {
+  const expression = getRandomInt(0, 100);
+  const correctAnswer = expression % 2 === 0 ? 'yes' : 'no';
+
+  return [expression, correctAnswer];
+};
 
 export default () => {
   const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  const reloadData = () => {
-    const expression = getRandomInt(0, 100);
-    let result = null;
-
-    if (expression % 2 === 0) {
-      result = 'yes';
-    } else result = 'no';
-    return [expression, result];
-  };
-
-  const userName = greeting();
-  gameLauncher(userName, description, reloadData);
+  gameLauncher(description, generateData);
 };
