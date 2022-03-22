f49adeb582966271f7ba82baddb6c9683f236170
Artem M <artemmarin91@mail.ru>
2022-03-23 01:15:31 +0300

    refactoring src/games/brainPrimeLogic.js

--- src/games/brainPrimeLogic.js
+++ src/games/brainPrimeLogic.js
@@ -1,20 +1,23 @@
 import gameLauncher from '../index.js';
 import getRandomInt from '../getRandomInt.js';
-import greeting from '../cli.js';
+
+const isPrime = (num) => {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const generateData = () => {
+  const expression = getRandomInt(0, 100);
+  const correctAnswer = isPrime(expression) ? 'yes' : 'no';
+  return [expression, correctAnswer];
+};
 
 export default () => {
   const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-  const reloadData = () => {
-    const number = getRandomInt(0, 100);
-    const primes = [
-      2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
-      71, 73, 79, 83, 89, 97,
-    ];
-    const answer = primes.includes(number) ? 'yes' : 'no';
-    return [number, answer];
-  };
-
-  const userName = greeting();
-  gameLauncher(userName, description, reloadData);
+  gameLauncher(description, generateData);
 };
