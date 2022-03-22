a743f7c75a8cd8731bf964f66a88f15f582fce63
Artem M <artemmarin91@mail.ru>
2022-03-23 01:23:01 +0300

    fix src/index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ export default (gameRules, getData) => {
       console.log('Correct!');
     } else {
       console.log(`"${userAnswer}" is wrong answer ;( Correct answer was "${correctAnswer}"`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
