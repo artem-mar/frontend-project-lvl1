71b2a3dc79292173210a6c980e11b34d570c7de0
Artem M <artemmarin91@mail.ru>
2022-03-23 01:12:33 +0300

    refactoring src/games/brainCalcLogic

--- src/games/brainCalcLogic.js
+++ src/games/brainCalcLogic.js
@@ -1,35 +1,33 @@
 import gameLauncher from '../index.js';
 import getRandomInt from '../getRandomInt.js';
-import greeting from '../cli.js';
+
+const generateData = () => {
+  const a = getRandomInt(0, 10);
+  const b = getRandomInt(0, 10);
+  let expression = null;
+  let correctAnswer = null;
+
+  switch (getRandomInt(1, 3)) {
+    case 1:
+      expression = `${a} + ${b}`;
+      correctAnswer = `${a + b}`;
+      break;
+    case 2:
+      expression = `${a} - ${b}`;
+      correctAnswer = `${a - b}`;
+      break;
+    case 3:
+      expression = `${a} * ${b}`;
+      correctAnswer = `${a * b}`;
+      break;
+    default:
+      break;
+  }
+  return [expression, correctAnswer];
+};
 
 export default () => {
   const description = 'What is the result of the expression?';
 
-  const reloadData = () => {
-    const a = getRandomInt(0, 10);
-    const b = getRandomInt(0, 10);
-    let expression = null;
-    let result = null;
-
-    switch (getRandomInt(1, 3)) {
-      case 1:
-        expression = `${a} + ${b}`;
-        result = `${a + b}`;
-        break;
-      case 2:
-        expression = `${a} - ${b}`;
-        result = `${a - b}`;
-        break;
-      case 3:
-        expression = `${a} * ${b}`;
-        result = `${a * b}`;
-        break;
-      default:
-        break;
-    }
-    return [expression, result];
-  };
-
-  const userName = greeting();
-  gameLauncher(userName, description, reloadData);
+  gameLauncher(description, generateData);
 };
