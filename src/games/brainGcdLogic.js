f3b7245c222d6e507b58cfe816e0bd6339c6be24
Artem M <artemmarin91@mail.ru>
2022-03-23 01:14:45 +0300

    refactoring  src/games/brainGcdLogic.js

--- src/games/brainGcdLogic.js
+++ src/games/brainGcdLogic.js
@@ -1,25 +1,26 @@
 import gameLauncher from '../index.js';
 import getRandomInt from '../getRandomInt.js';
-import greeting from '../cli.js';
+
+const findGCD = (a, b) => {
+  let divisor = a < b ? a : b;
+
+  while (a % divisor !== 0 || b % divisor !== 0) {
+    divisor -= 1;
+  }
+  return divisor;
+};
+
+const generateData = () => {
+  const firstNum = getRandomInt(1, 100);
+  const secondNum = getRandomInt(1, 100);
+  const expression = `${firstNum} ${secondNum}`;
+  const correctAnswer = findGCD(firstNum, secondNum).toString();
+
+  return [expression, correctAnswer];
+};
 
 export default () => {
   const description = 'Find the greatest common divisor of given numbers.';
 
-  const reloadData = () => {
-    const a = getRandomInt(0, 100);
-    const b = getRandomInt(0, 100);
-    const expression = `${a} ${b}`;
-
-    let divider = a < b ? a : b;
-
-    while (a % divider !== 0 || b % divider !== 0) {
-      divider -= 1;
-    }
-    divider = divider.toString();
-
-    return [expression, divider];
-  };
-
-  const userName = greeting();
-  gameLauncher(userName, description, reloadData);
+  gameLauncher(description, generateData);
 };
