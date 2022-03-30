2e2f71e5943ad33e03cad8a5f24a7b579c5bce22
Artem M <artemmarin91@mail.ru>
2022-03-30 03:02:10 +0300

    fix src/games/brainPrimeLogic.js

--- src/games/brainPrimeLogic.js
+++ src/games/brainPrimeLogic.js
@@ -2,6 +2,10 @@ import gameLauncher from '../index.js';
 import getRandomInt from '../getRandomInt.js';
 
 const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
